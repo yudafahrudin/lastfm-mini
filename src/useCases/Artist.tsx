f88e6ad2fb2847e47203642460e8a2a23192ca01
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-13 22:09:45 +0700

    refactor: add search logic in artistUseCase

--- src/useCases/Artist.tsx
+++ src/useCases/Artist.tsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from "react";
 
 // resource
-import { artistTopResource } from "@/infrastructure/resources/ArtistResource";
+import {
+  artistTopResource,
+  artisSearchResource,
+} from "@/infrastructure/resources/ArtistResource";
 
 // domain type
 import { ArtistType } from "@/domains/Artist";
+import { debounce } from "@mui/material";
 
 export const useArtistCase = () => {
   const [artistList, setArtistList] = useState<ArtistType[]>();
+  const [loading, setLoading] = useState(false);
 
-  const getArtistTopListResource = async () => {
+  const getArtistTopList = async () => {
+    setLoading(true);
     setArtistList((await artistTopResource()).artists.artist);
+    setLoading(false);
   };
 
+  const handleArtistSearch = debounce(async (value) => {
+    if (value) {
+      setLoading(true);
+      setArtistList(
+        (await artisSearchResource(value))?.results?.artistmatches?.artist || []
+      );
+      setLoading(false);
+    } else {
+      getArtistTopList();
+    }
+  }, 500);
+
   useEffect(() => {
-    getArtistTopListResource();
+    getArtistTopList();
   }, []);
 
   return {
+    loading,
     artistList,
+    handleArtistSearch,
   };
 };
