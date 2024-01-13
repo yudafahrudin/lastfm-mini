fd14575704dd2bff5b90eeab243fd684e402ade6
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-13 22:07:16 +0700

    refactor: add menu page

--- src/components/DefaultWrapper.tsx
+++ src/components/DefaultWrapper.tsx
@@ -6,7 +6,9 @@ import {
   Toolbar,
   useTheme,
   Button,
+  Box,
 } from "@mui/material";
+import { MusicNoteOutlined } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 interface Props {
@@ -29,13 +31,22 @@ const DefaultWrapper: React.FC<Props> = ({ children, leftIcon, rightIcon }) => {
   const theme = useTheme();
 
   return (
-    <Stack spacing={2}>
+    <Stack spacing={2} mb={5}>
       <Stack direction="row">
         <ThemeProvider theme={darkTheme}>
           <AppBar color="default" position="static">
             <Toolbar>
-              {leftIcon || <Button>SOM</Button>}
-              <Typography>let's find out your song</Typography>
+              {leftIcon || (
+                <Box mr={2}>
+                  <MusicNoteOutlined />
+                </Box>
+              )}
+              <Button color="inherit" variant="text">
+                <Typography>Top Artists</Typography>
+              </Button>
+              <Button color="inherit" variant="text">
+                <Typography>Top Songs</Typography>
+              </Button>
               <Stack direction="row" spacing={1} sx={{ marginLeft: "auto" }}>
                 <Stack direction="row" spacing={1}>
                   {rightIcon}
