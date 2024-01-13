3012387c8e33288ffbed5a39400f287388eedab8
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-13 16:22:57 +0700

    refactor: :fire: implement artist useCase and data

--- src/presentations/Home.tsx
+++ src/presentations/Home.tsx
@@ -1,18 +1,72 @@
 "use client";
 
 import React from "react";
-import { Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Stack, Grid, Box, Container } from "@mui/material";
+
+import Divider from "@mui/material/Divider";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+import { useArtistCase } from "@/useCases/Artist";
 
 const HomePresenter: React.FC = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const { artistList } = useArtistCase();
 
   return (
-    <Stack spacing={3} textAlign="center">
-      <Typography variant={isMobile ? "subtitle1" : "h2"}>
-        Play song list
-      </Typography>
-    </Stack>
+    <Container>
+      <Stack mb={5} spacing={2}>
+        <Typography variant="h3">Top 50 Artist</Typography>
+      </Stack>
+
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        rowSpacing={2}
+      >
+        <Grid item xs={0.5} />
+        <Grid item xs={1} />
+        <Grid item xs={5.5}>
+          <Typography variant="subtitle2">Name</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="subtitle2">Listener</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="subtitle2">Play Count</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+        {artistList?.map((data, index) => {
+          return (
+            <>
+              <Grid item xs={0.5}>
+                {index + 1}
+              </Grid>
+              <Grid item xs={1}>
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src={data.image[0]["#text"]} />
+                </ListItemAvatar>
+              </Grid>
+              <Grid item xs={5.5}>
+                <Typography variant="subtitle1"> {data.name}</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                {data.listeners}
+              </Grid>
+              <Grid item xs={2}>
+                {data.playcount}
+              </Grid>
+              <Grid item xs={12}>
+                <Divider />
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
